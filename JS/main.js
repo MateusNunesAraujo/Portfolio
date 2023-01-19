ff337c8f00249f03160ac5d2f14ecf34f0aa20c1
MateusNunesAraujo <118635721+MateusNunesAraujo@users.noreply.github.com>
2023-01-19 16:59:54 -0500

    Delete JS directory

--- JS/main.js
+++
@@ -1,8 +0,0 @@
-
-    let age = document.getElementById("age")
-    const fecha = Date.now()
-    const hoy = new Date(fecha)
-    const year = hoy.getFullYear()
-    let edad = year -2005
-    age.innerHTML = edad
-
